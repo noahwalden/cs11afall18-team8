5679fe7b302a61a2b8883838fe5d6be676d86f48
Noah Walden <noahwalden@brandeis.edu>
2018-11-27 10:49:34 -0500

    Update GameInput.java

--- GameInput.java
+++ GameInput.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 public class GameInput {
-  public static void main(String[] args) {
+  public static void prisonCell(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     System.out.println("> ");
     String input = myScanner.nextLine();
     switch(input) {
       case "north":
       case "n":
-        return input; break;
+        return "n"; break;
       case "east":
       case "e":
+        return "e"; break;
       case "south":
       case "s":
+        return "s"; break;
       case "west":
       case "w":
+        return "w"; break;
+      case ""
     }
+  }
 
+  public static void error() {
+    System.out.println("Sorry, you can't do that.");
   }
 }
