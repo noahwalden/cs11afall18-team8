ed86638f28f6122d1b8dfdb7b7c2a2df3b915928
Martin Sipowicz <martinsipowicz@brandeis.edu>
2018-11-27 10:43:52 -0500

    Update RoomDescriptions.java

--- RoomDescriptions.java
+++ RoomDescriptions.java
@@ -6,13 +6,19 @@ public class RoomDescriptions {
   public static void prisonCell(String[] args) {
     System.out.println("Prison Cell");
     System.out.println("The cell is dark and covered in alien slime.");
-    System.out.println("The cell door, slightly ajar, is located to the east.");
-    
+    System.out.println("The cell door, slightly ajar, is located to the north.");
   }
 }
 // room 1
-public static void prisonCell(String[] args) {
+public static void Barracks(String[] args) {
   System.out.println("Barracks");
-  System.out.println("The barracks have many futuristic weapons you decide to go with a pipe");
+  System.out.println("The barracks have many futuristic weapons these weapons include plasma torpedos, bananas, and pipes");
   System.out.println("There is a a door strait to the south of you and two doors that are to the east and west of you");
+}
+
+// room 7
+public static void (String[] args) {
+  System.out.println("1st Alien Room");
+  System.out.println("This room seems to be empty except for one thing in the corner that seems to be breathing");
+  System.out.println("You can either return return east or move west risking waking the thing in the corner.");
 }
