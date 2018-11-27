42b7bd9128cfb18c63120e45232048b00be78570
Martin Sipowicz <martinsipowicz@brandeis.edu>
2018-11-27 10:06:28 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ public class Game {
   public static void main(String[] args) {
     introSequence()
   }
-  public static void intoSequence(String[] args) {
+  public static void introSequence(String[] args) {
     System.out.println("The Boolin' Operators' Text Adventure Game");
     System.out.println("Professor: Timothy Hickey");
     System.out.println("Created on Nov. 27, 2018");
