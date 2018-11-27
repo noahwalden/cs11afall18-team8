518c82c4498a983cabdd1ecd228509beb349b143
Charles Beyrard <beyrardcharles@gmail.com>
2018-11-27 10:26:13 -0500

    Update RoomDescriptions.java

--- RoomDescriptions.java
+++ RoomDescriptions.java
@@ -7,7 +7,6 @@ public class RoomDescriptions {
     System.out.println("Prison Cell");
     System.out.println("The cell is dark and covered in alien slime.");
     System.out.println("The cell door, slightly ajar, is located to the east.");
-    
   }
 }
 // room 1
@@ -15,4 +14,11 @@ public static void prisonCell(String[] args) {
   System.out.println("Barracks");
   System.out.println("The barracks have many futuristic weapons you decide to go with a pipe");
   System.out.println("There is a a door strait to the south of you and two doors that are to the east and west of you");
+}
+//room 5
+public static void surveillanceRoom(String[] agrs) {
+  System.out.println("Surveillance room:");
+  System.out.println("You enter a room filled with screens of what seems to be the ship.")
+  System.out.println("You constantly notice alien symbols which you can comprehend.");
+  System.out.println("You see a red button, what to do?");
 }
