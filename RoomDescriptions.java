77d875622e8670ab12e39f24f61b1a31a443cc1d
Noah Walden <noahwalden@brandeis.edu>
2018-11-27 10:05:26 -0500

    Update RoomDescriptions.java

--- RoomDescriptions.java
+++ RoomDescriptions.java
@@ -1,5 +1,11 @@
+/*
+Room 7 - prison cell
+*/
+
 public class RoomDescriptions {
-  public static void main(String[] args) {
-    
+  public static void prisonCell(String[] args) {
+    System.out.println("Prison Cell");
+    System.out.println("The cell is dark and covered in alien slime.");
+    System.out.println("The cell door, slightly ajar, is located to the north.");
   }
 }
