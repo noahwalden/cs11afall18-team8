80df7753608fdce7f1b1cba5eef8fe443d5587d5
Charles Beyrard <beyrardcharles@gmail.com>
2018-11-27 11:09:59 -0500

    Update GameInput.java

--- GameInput.java
+++ GameInput.java
@@ -19,15 +19,19 @@ public class GameInput {
 
   // room 5 choices
   public static void Choice5(String[] args) {
-    String input = myScanner.nextLine();
     System.out.println("You see a red button, what to do?");
+    String input = myScanner.nextLine();
     System.out.println("To press the button, press p. To leave it alone, press l");
     switch(input) {
       case "p":
-        System.out.println("All the cameras are now shut down, well done.");
-      case "l": 
-        System.out.println("You must now ")
+        System.out.println("All the cameras are now shut down");
+      case "l":
+        System.out.println("You notice guards may be coming your way, it's time to go");
     }
+    System.out.println("You see another door going South.");
+    System.out.println("To enter the new room, press S.");
+    System.out.println("To return to the Barracks, press B.");
+    String input = myScanner.nextLine();
 
   }
 }
