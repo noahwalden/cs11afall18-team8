c32385e8210835ab52995a173d6b9f33e3a59b77
Noah Walden <noahwalden@brandeis.edu>
2018-11-30 14:13:15 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 public class Game {
   public static void main(String[] args) {
     introSequence();
     prisonCell();
   }
-  public static void introSequence(String[] args) {
+  private static void introSequence(String[] args) {
     System.out.println("The Boolin' Operators' Text Adventure Game");
     System.out.println("Professor: Timothy Hickey");
-    System.out.println("Created on Nov. 27, 2018");
+    System.out.printf("Created on Nov. 27, 2018%n");
+
+    Scanner ms = new Scanner(System.in);
+    System.out.println("Show intro sequence? (y/n): ");
+    String input = ms.nextLine();
+    if(input.equals("y")) {
+      backstory();
+    }
   }
   public static void prisonCell(String[] args) {
     RoomDescriptions.prisonCell();
@@ -15,4 +23,13 @@ public class Game {
       case "e": barracks();
       default: GameInput.error();
     }
+
+  private static void backstory() {
+    System.out.printf("Superbowl Sunday, 1995. You and your buddies had all%n
+                      gathered at your house to watch the big game. At halftime,%n
+                      you decide to leave the house and swing by the store for%n
+                      more chips and dip. As you walk back from the convenience%n
+                      store to the car, a bright suddenly appears directly above%n
+                      your head, blinding you.");
+  }
 }
