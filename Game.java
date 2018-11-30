b333433c0f67bb1da004afc6e70ad043910583f5
Lisandro Mayancela <lmayancela08@gmail.com>
2018-11-30 15:08:21 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,16 +3,17 @@ public class Game {
     introSequence();
     prisonCell();
   }
-  public static void introSequence(String[] args) {
+  public static void introSequence() {
     System.out.println("The Boolin' Operators' Text Adventure Game");
     System.out.println("Professor: Timothy Hickey");
     System.out.println("Created on Nov. 27, 2018");
   }
-  public static void prisonCell(String[] args) {
+  public static void prisonCell() {
     RoomDescriptions.prisonCell();
     String userInput = GameInput.prisonCell();
     switch(userInput) {
       case "e": barracks();
       default: GameInput.error();
     }
+  }
 }
