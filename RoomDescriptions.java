90f39673a8b8974de75291d8291fffa496043b81
Noah Walden <noahwalden@brandeis.edu>
2018-11-30 14:13:12 -0500

    Update RoomDescriptions.java

--- RoomDescriptions.java
+++ RoomDescriptions.java
@@ -16,7 +16,7 @@ public static void Barracks(String[] args) {
   System.out.println("There is a a door strait to the south of you and two doors that are to the east and west of you");
 }
 //room 5
-public static void surveillanceRoom(String[] agrs) {
+public static void surveillanceRoom(String[] args) {
   System.out.println("Surveillance room:");
   System.out.println("You enter a room filled with screens of what seems to be the ship.")
   System.out.println("You notice alien symbols which you can comprehend.");
