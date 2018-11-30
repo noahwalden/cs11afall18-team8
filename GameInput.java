885b243f3e4ec1f27c23df606b4a9894f5977cdd
Noah Walden <noahwalden@brandeis.edu>
2018-11-30 15:55:31 -0500

    Update GameInput.java

--- GameInput.java
+++ GameInput.java
@@ -6,7 +6,7 @@ public class GameInput {
     String x = myScanner.nextLine().toLowerCase();
     while ((!x.equals("a")) && (!x.equals("b")) && (!x.equals("c")) && (!x.equals("d"))) {
       System.out.println("That answer is not valid. Please try again.");
-      System.out.println("> ");
+      System.out.print("> ");
       x = myScanner.nextLine().toLowerCase();
     }
     return x;
