adb6c57c7177a54212d1353b4fae41a0c2536f15
Noah Walden <noahwalden@brandeis.edu>
2018-11-30 16:03:05 -0500

    Update GameInput.java

--- GameInput.java
+++ GameInput.java
@@ -11,19 +11,4 @@ public class GameInput {
     }
     return x;
   }
-
-  // room 5 action
-  public static void Choice5(String[] args) {
-    Scanner myScanner = new Scanner(System.in);
-    System.out.println("You see a red button, what to do?");
-    System.out.println("To press the button, press p. To leave it alone, press l");
-    String input = myScanner.nextLine();
-    switch(input) {
-      case "p":
-        System.out.println("All the cameras are now shut down, well done.");
-      case "l":
-        System.out.println("You must now ");
-    }
-
-  }
 }
