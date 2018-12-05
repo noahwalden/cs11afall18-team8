6375c4d8d57ff682d17173b086b385da3f9498c3
elijahmiller237 <elijahmiller237@gmail.com>
2018-12-04 23:47:39 -0500

    Update Room3.java

--- Room3.java
+++ Room3.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
+/* Author: Elijah Miller
+This is the cafeteria.
 
+
+
+There is no violence in the cafeteria
+*/
 public class Room3 {
 
-  public static boolean hasExplored = false;
+  public static boolean hasExplored = false; //whether or not the player has explored this room
 
   public static void room3() {
     System.out.println();
@@ -17,7 +23,7 @@ public class Room3 {
       System.out.println();
       firstChoice();
     }
-  }
+  } //end of method
 
   private static void firstChoice() {
     boolean hasLooked = false;
@@ -57,7 +63,7 @@ public class Room3 {
         getYams();
       }
     }
-  }
+  } //end of method
 
   private static void lookAround() {
     System.out.println("You look around and see a pumpkin on the table.");
@@ -79,13 +85,13 @@ public class Room3 {
     if (answer.equals('a' + "")) {
       Inventory.addToInventory("pumpkin");
     }
-  }
+  } //end of method
 
   private static void sitDown() {
     System.out.println("You get yourself a piece of strudel and");
     System.out.println("have a nice lunch with the alien.");
     leaveRoom();
-  }
+  } //end of method
 
   private static void getYams() {
     int yams = (int)Math.floor(Math.random()*6.0);
@@ -94,10 +100,10 @@ public class Room3 {
     System.out.println("Congradulations.");
     System.out.println();
     leaveRoom();
-  }
+  } //end of method
 
-  private static void leaveRoom() {
-    hasExplored = true;
+  private static void leaveRoom() { //method to bring up the exit options
+    hasExplored = true; //hasExplored is assigned the value true once the player decides to leave the room
     System.out.println("Which door do you want to leave through?");
     System.out.println("  A) The Northern door");
     System.out.println("  B) The Western door");
@@ -115,10 +121,10 @@ public class Room3 {
     }
     Combat.randomEncounter();
     if (answer.equals('a' + "")) {
-      Room6.room6();
+      Room6.room6(); //call to room 6
     }
     else {
-      Room2.room2();
+      Room2.room2(); //call to room 2
     }
-  }
-}
+  } //end of method
+} //end of class
